2374163f42aa2e312c699807cf169f46c0f10bc5
a1mp <ignaciozp21@gmail.com>
2025-03-26 20:16:37 -0600

    Version 1.0.1 de base de datos

--- Inicio/src/main/java/com/mycompany/inicio/ConexionDB.java
+++ Inicio/src/main/java/com/mycompany/inicio/ConexionDB.java
@@ -1,12 +1,41 @@
 package com.mycompany.inicio;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
-public class BaseDeDatos {
+public class ConexionDB {
     private Connection conexion;
     private PreparedStatement consulta;
     private ResultSet resultado;
-    private static final String url = ""
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "123456789";
+
+    public static Connection getConexiox() {
+        Connection conexion = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conexion = DriverManager.getConnection(URL, NAME, PASSWORD);
+            System.out.println("Conexion con exito");
+        } catch (ClassNotFoundException e){
+            System.out.println("Driver JDBC no encontrado");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Conexion fallida:\nError: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+    public static void cerrarConexion(Connection conexion) {
+        if (conexion != null) {
+            try     {
+                conexion.close();
+                System.out.println("Se desconecto la base de datos");
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
